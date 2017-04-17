120a949b1ff1deababecdf767d5561f6d5d23547
Baelyk <b43lyk@gmail.com>
2017-04-16 19:23:46 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Dwarf Fortress Launcher
+
+Dwarf Fortress Launcher is a launcher that I built with [Electron](http://electron.atom.io), half for fun and half to learn Electron. You can launch Dwarf Fortress (it would kinda suck as a launcher if you couldn't), edit almost all the configs, change fonts and tilesets, and backup saves and configurations.
+
+It's still not done and needs some further work, but it does work. I wouldn't use it with an important DF install though, maybe create an install just for this because although it shouldn't mess anything up, I haven't done extensive enough testing to know for sure.
+
+## Features
+
+- Launch Dwarf Fortress
+- Edit basic configs
+- Edit almost all of the configs in `init.txt` and `d_init.txt`
+- Change fonts and tilesets
+- Backup and restore saves and configurations
+
+## Planned features
+
+- Choose which configs are "basic"
+- Install DF through the launcher
+- Manage multiple versions
+- Add fonts and tilesets to the data directory through the launcher (not manually moving them there)
+- Move all paths to use the `path` module
---
+++ launcher/assets/js/firststartup.js
@@ -0,0 +1,78 @@
+/*
+This file is for firststartup.html, when the app is first started and needs to
+be configured
+*/
+
+const ipc = require("electron").ipcRenderer // Not be a hermit
+
+const log = { // This is the logging function so that I get logs when testing and in "production"
+    error: function (msg) {
+        ipc.send("log-error", msg)
+    },
+    warn: function (msg) {
+        ipc.send("log-warn", msg)
+    },
+    info: function (msg) {
+        ipc.send("log-info", msg)
+    },
+    verbose: function (msg) {
+        ipc.send("console.log", msg)
+    },
+    debug: function (msg) {
+        ipc.send("console.log", msg)
+    },
+    silly: function (msg) {
+        ipc.send("log-silly", msg)
+    },
+}
+
+let datadiv = document.querySelector("#data")
+let dfdiv = document.querySelector("#df")
+let restartdiv = document.querySelector("r#estart")
+let restartbtn = document.querySelector("#restart button")
+let databtn = document.querySelector("#data button")
+let dfbtn = document.querySelector("#df button")
+let datadirspan = document.querySelector("#data-dir")
+let dfdirspan = document.querySelector("#df-dir")
+
+let datadir, datadirname, dfdir, dfdirnamr
+
+// Functions -------------------------------------------------------------------
+
+// For IPC events
+
+function dirSelected (e, directory, dir, dirName) {
+    if (directory) {
+        datadir = dir
+        datadirname = dirName
+        datadirspan.innerHTML = `<span class="bold">${dirName}:</span> ${dir}`
+    } else if (directory === false) {
+        dfdir = dir
+        dfdirname = dirName
+        dfdirspan.innerHTML = `<span class="bold">${dirName}:</span> ${dir}`
+    }
+}
+
+// IPC events ------------------------------------------------------------------
+
+ipc.on("dir-selected", dirSelected)
+
+// Event listeners -------------------------------------------------------------
+
+databtn.addEventListener("click", function () {
+    ipc.send("dirbtn-click", true)
+})
+
+dfbtn.addEventListener("click", function () {
+    ipc.send("dirbtn-click", false)
+})
+
+restartbtn.addEventListener("click", function () {
+    if(datadir === undefined || datadirname === undefined || dfdir === undefined || dfdirname === undefined) {
+        console.log("Startup failed because one of the directories (or its name) value was undefined.")
+        // ipc.send("startup-fail")
+    } else {
+        console.log("Startup succeeded (because you followed the directions)!")
+        ipc.send("startup-succeed", [datadir, datadirname], [dfdir, dfdirname])
+    }
+})
